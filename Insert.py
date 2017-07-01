30810666c053e38ccbd18b15a618e0c49eb83fc8
dsilva25 <dsilvacontreras25@gmail.com>
2017-06-30 21:19:56 -0300

    Algoritmo InsertSort

---
+++ Insert.py
@@ -0,0 +1,33 @@
+#Algoritmo Insercion leyendo datos desde un archivo
+
+def insercion(s):
+    for i in range(1, len(s)):
+        val = s[i]
+        j = i - 1
+        while (j >= 0) and (s[j] > val):
+            s[j+1] = s[j]
+            j = j - 1
+        s[j+1] = val
+ 
+infile = open ("numerosAleatorios.txt", "r") 
+file=infile.readlines() 
+lista=[]
+
+for linea in file: 
+   var=linea.strip() #depurar salto de linea
+   x=var 
+   lista.append(int(x)) 
+
+infile.close()
+
+## ----------------------- Ejecutamos QuickSort----------------------
+insercion(lista)
+## ---------------- Escribimos variables en otro txt-----------------
+infile2=open('numOrdenados.txt','w')
+
+for linea2 in lista:
+   infile2.write(str(linea2)+'\n')
+
+infile2.close()
+
+print ("Archivo numOrdenados.txt generado")
